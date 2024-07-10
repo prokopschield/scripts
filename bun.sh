52e25034913268be13ed4b9873fe8e656d5a7b36
Prokop <prokop.schield@protonmail.com>
2024-07-10 03:35:27 +0200

    fix(bun.sh): install unzip

--- bun.sh
+++ bun.sh
@@ -8,6 +8,14 @@ if ! [ -n `which curl` ]; then
 	fi
 fi
 
+## install unzip
+if ! [ -n `which unzip` ]; then
+	if ! apt install unzip -y; then
+		echo Cannot install unzip. >&2
+		exit 1
+	fi
+fi
+
 ## install bun
 if ! [ -n `which bun` ]; then
 	if ! curl -fsSL https://bun.sh/install | bash; then
